0d153fb1de203076dfacb2c38ba21a9f4f4c919c
jamcha <jamcha.aa@gmail.com>
2019-06-01 10:14:48 +0900

    Mod data/users.js

--- website/data/users.js
+++ website/data/users.js
@@ -140,5 +140,12 @@ module.exports = [
     fbOpenSource: false,
     pinned: false,
   },
+  {
+    caption: 'ウェブの森で迷子になった話。',
+    image: 'https://rawcdn.githack.com/jamcha-aa/About/494af5169967aebf05ced683bbfd3b860d5db0aa/website/static/img/users/hugo.png',
+    infoLink: 'https://jamcha-aa-hugo.netlify.com/',
+    fbOpenSource: false,
+    pinned: false,
+  },
   // Please add your logo in alphabetical order of caption.
 ];
